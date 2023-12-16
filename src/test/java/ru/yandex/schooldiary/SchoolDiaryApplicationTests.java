2784dc09a94d5768def7062424d18eef99153183
ViktorB <vitekb650@gmail.com>
2023-12-16 19:17:37 +0300

    init commit

---
+++ src/test/java/ru/yandex/schooldiary/SchoolDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.schooldiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
